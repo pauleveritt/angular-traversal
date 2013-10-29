1aa4f04692d2bc3a82178f11109731f711e1bb2d
Paul Everitt <paul@agendaless.com>
2013-10-29 15:04:42 -0400

    Get started on revamp.

--- provider/app.js
+++ provider/app.js
@@ -1,8 +1,21 @@
-var app = angular.module("app", ['traversal']);
+var app = angular.module("traversalApp", ['ngRoute', 'traversal']);
 
-app.config(function (gameProvider) {
-    gameProvider.setType("Peace");
+app.config(function ($routeProvider, gameProvider) {
+    gameProvider.setType("Peaceful");
+
+    $routeProvider.when('/',
+                        {
+                            templateUrl: "partial1.html",
+                            controller: "AppCntrl",
+                            resolve: {
+                                init: function ($q) {
+                                    var defer = $q.defer();
+                                    defer.resolve();
+                                    return defer.promise;
+                                }}
+                        })
 });
+
 
 app.controller("AppCntrl", function ($scope, game) {
     $scope.title = game.title;
