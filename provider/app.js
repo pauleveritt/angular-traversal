8d9617e0c2b3e952207e9060c8cab4a9bf65cdcb
Paul Everitt <paul@agendaless.com>
2013-10-31 08:15:43 -0400

    Moved controllers into own module.

--- provider/app.js
+++ provider/app.js
@@ -6,68 +6,23 @@ app.config(function ($stateProvider) {
         .state('layout', {
                    abstract: true,
                    templateUrl: 'partials/layout.html',
-                   controller: "LayoutView"
+                   controller: LayoutView
 
                })
         .state('siteroot-default', {
                    url: '*path',
                    parent: "layout",
-                   controller: "SiteRootView",
+                   controller: SiteRootView,
                    templateUrl: "partials/siteroot_view.html"
                })
         .state('folder-default', {
                    parent: "layout",
-                   controller: "FolderView",
+                   controller: FolderView,
                    templateUrl: "partials/folder_view.html"
                })
         .state('document-default', {
                    parent: "layout",
-                   controller: "DocumentView",
+                   controller: DocumentView,
                    templateUrl: "partials/document_view.html"
                })
-});
-
-
-app.controller("LayoutView", function ($http, $scope, traverser) {
-
-    function assign_label(this_resource) {
-        this_resource.label = "xyz";
-    }
-
-    $scope.load_data = function () {
-        $http.get('site_data.json')
-            .success(function (data) {
-                         traverser.setRoot(data, [assign_label]);
-                     })
-            .error(function () {
-                       console.log('error loading data');
-                   })
-    };
-    $scope.load_data();
-
-    /* When the traverser changes (data loaded, user goes to a
-     different location), recalculate the breadcrumbs */
-    $scope.$on("traverserChanged", function (event, context) {
-
-        // Calculate breadcrumbs
-        var breadcrumbs = [];
-        traverser.parents.forEach(function (item) {
-            breadcrumbs.push(item)
-        });
-        breadcrumbs.push(context);
-        $scope.breadcrumbs = breadcrumbs;
-
-    });
-});
-
-app.controller("SiteRootView", function () {
-    console.log("SiteRootView");
-});
-
-app.controller("FolderView", function () {
-    console.log("FolderView");
-});
-
-app.controller("DocumentView", function () {
-    console.log("DocumentView");
 });
---
+++ provider/controllers.js
@@ -0,0 +1,45 @@
+var app = angular.module("traversalApp", []);
+
+function LayoutView ($http, $scope, traverser) {
+
+    function assign_label(this_resource) {
+        this_resource.label = "xyz";
+    }
+
+    $scope.load_data = function () {
+        $http.get('site_data.json')
+            .success(function (data) {
+                         traverser.setRoot(data, [assign_label]);
+                     })
+            .error(function () {
+                       console.log('error loading data');
+                   })
+    };
+    $scope.load_data();
+
+    /* When the traverser changes (data loaded, user goes to a
+     different location), recalculate the breadcrumbs */
+    $scope.$on("traverserChanged", function (event, context) {
+
+        // Calculate breadcrumbs
+        var breadcrumbs = [];
+        traverser.parents.forEach(function (item) {
+            breadcrumbs.push(item)
+        });
+        breadcrumbs.push(context);
+        $scope.breadcrumbs = breadcrumbs;
+
+    });
+};
+
+function SiteRootView () {
+    console.log("SiteRootView");
+};
+
+function FolderView () {
+    console.log("FolderView");
+};
+
+function DocumentView () {
+    console.log("DocumentView");
+};
